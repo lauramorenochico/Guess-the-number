79d324eeb15fd9a659a1f3e471edfd39febdc6d6
lauramorenochico <laura.moreno.chico@gmail.com>
2023-02-14 17:41:36 +0100

    add 6 of 10 task

--- js/main.js
+++ js/main.js
@@ -5,13 +5,15 @@ const secNumber = document.querySelector(".js-section-number");
 const btn = document.querySelector(".js-section-btn");
 const clueNumber = document.querySelector(".js-clue");
 const counter = document.querySelector(".js-counter");
+let counterMoves = 0; //se deja fuera porque no queremos que se ejecute cada vez que hacemos click
+const randomNumber = getRandomNumber(100); //se deja fuera porque no queremos que se ejecute cada vez que hacemos click
 
 // cuando hacemos click en botón , se genera un número aleatorio que compararemos con el dado por la jugadora
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-const randomNumber = getRandomNumber(100); //se deja fuera porque no queremos que se ejecute cada vez que hacemos click
+
 console.log(randomNumber);
 
 function pcRandomNumber() {
@@ -28,7 +30,7 @@ function pcRandomNumber() {
 }
 
 //actualizar el contador
-let counterMoves = 0; //se deja fuera porque no queremos que se ejecute cada vez que hacemos click
+
 function countMoves() {
   const totalMoves = counterMoves++;
   counter.innerHTML = `Número de intentos: ${totalMoves}`;
